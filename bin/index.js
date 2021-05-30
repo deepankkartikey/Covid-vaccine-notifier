c7c4399ace7d920464e6fad9ae9c7f02b82b60af
Deepank kartikey <deepankkartikey@gmail.com>
2021-05-31 00:02:09 +0530

    Added logic for slots

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,9 @@
 const states = require('../util/states');
 const districts = require('../util/districts');
+const slots = require('../util/slots');
 
 // states();
 
-districts(1);
+//districts(1);
+
+slots(15);
---
+++ util/slots.js
@@ -0,0 +1,84 @@
+const axios = require('axios');
+const Table = require('tty-table');
+const { config, options } = require('./config');
+
+const getSlotsByDistrictUrl =
+	'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict';
+
+const header = [
+	{
+		value: 'center',
+		headerColor: 'cyan',
+		color: 'white',
+		align: 'left',
+		width: 30,
+		alias: 'Center Name',
+	},
+	{
+		value: 'address',
+		color: 'red',
+		width: 25,
+		alias: 'Center Address',
+	},
+	{
+		value: 'available',
+		color: 'red',
+		width: 25,
+		alias: 'Available Slots',
+	},
+	{
+		value: 'age',
+		color: 'red',
+		width: 5,
+		alias: 'Age',
+	},
+	{
+		value: 'date',
+		color: 'red',
+		width: 15,
+		alias: 'Date',
+	},
+];
+
+module.exports = function (districtId) {
+	var todayDate = new Date();
+	var dd = String(todayDate.getDate()).padStart(2, '0');
+	var mm = String(todayDate.getMonth() + 1).padStart(2, '0'); //January is 0!
+	var yyyy = todayDate.getFullYear();
+	var formattedDate = `${dd}-${mm}-${yyyy}`;
+	console.log(formattedDate);
+	axios
+		.get(
+			getSlotsByDistrictUrl +
+				`?district_id=${districtId}
+                &date=${formattedDate}`,
+			config
+		)
+		.then(function (response) {
+			const centerData = response.data.centers;
+			var finalData = [];
+			// const availableCenters = Table(header, centerData, options).render();
+			centerData.forEach(item => {
+				item.sessions.forEach(session => {
+					//console.log(session);
+					let ourData = {
+						center: item.name,
+						address: item.address,
+						available: session.available_capacity,
+						age: session.min_age_limit,
+						date: session.date,
+						dose1: session.available_capacity_dose1,
+						dose2: session.available_capacity_dose2,
+					};
+					//console.log(ourData);
+					finalData.push(ourData);
+				});
+			});
+			const finalSlotData = Table(header, finalData, options).render();
+			console.log(finalSlotData);
+		})
+		.catch(function (error) {
+			console.log(error);
+		})
+		.then(function () {});
+};
