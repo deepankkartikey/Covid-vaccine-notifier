99b25ec9ae4bfd2d90f6c689516950c88d1d3312
Deepank kartikey <deepankkartikey@gmail.com>
2021-05-31 11:15:06 +0530

    Add commands for CLI using Commander

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,24 @@
+#! /usr/bin/env node
+
 const states = require('../util/states');
 const districts = require('../util/districts');
 const slots = require('../util/slots');
+const program = require('commander');
+
+program.command('states').description('get list of all states').action(states);
+
+program
+	.command('districts <stateid>')
+	.description('get list of all districts by stateid')
+	.action(districts);
+
+program
+	.command('slot <districtid>')
+	.description('get list of all available slots by districtid')
+	.action(slots);
+
+program.parse();
 
 // states();
-
-//districts(1);
-
-slots(15);
+// districts(1);
+// slots(15);
